ee5ef323bade9c2b8ed55deeea65c100ad481ce2
escanor550 <naroto_07@hotmail.com>
2023-05-16 14:29:45 +0300

    hi ffff

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Models;
+
+namespace ReverseNameApp.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult About(string name)
+        {
+            var reversedName = ReverseString(name);
+            return View("About", reversedName);
+        }
+
+        private string ReverseString(string input)
+        {
+            var charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
