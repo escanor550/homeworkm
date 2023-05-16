8c02a481d05efe343c8a40a67a4fa044d19f31fe
escanor550 <naroto_07@hotmail.com>
2023-05-16 14:45:27 +0300

    sentfille

---
+++ WebApplication6/WebApplication6/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication6.Models;
+
+namespace CalculatorApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult About(int number1, int number2)
+        {
+            ViewData["Number1"] = number1;
+            ViewData["Number2"] = number2;
+            ViewData["Addition"] = number1 + number2;
+            ViewData["Multiplication"] = number1 * number2;
+            ViewData["Subtraction"] = number1 - number2;
+            ViewData["Division"] = number1 / number2;
+
+            return View();
+        }
+    }
+}
